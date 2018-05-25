e92e423eaf79ad1fbbaa1c98947c7ce9e31e5d1f
Monkeli-Fred DILAPISHO <mdilapi@e5r5p1.wethinkcode.co.za>
2018-05-25 13:51:50 +0200

    problem is improper termination of strings

--- get_next_line.c
+++ get_next_line.c
@@ -30,8 +30,20 @@ static int	found_nl(const char *s)
 	}
 	return (-1);
 }
-
-static int gnl(char *ret, char **tmp, const int fd)
+static int	cat_join(size_t *i, int *count, char **tmp, char *ret)
+{
+	if (*i == (size_t)(BUFF_SIZE * *count - 1))
+	{
+		*count = *count + 1;
+		if ((*tmp = ft_strjoin(*tmp, ret)) == NULL)
+			return (-1);
+	} else 
+	{
+		*tmp = ft_strncat(*tmp, ret, BUFF_SIZE * *count + 3);
+	}
+	return (1);
+}
+static int	gnl(char *ret, char **tmp, const int fd)
 {
 	int		count;
 	int		new_line;
@@ -41,7 +53,8 @@ static int gnl(char *ret, char **tmp, const int fd)
 	count = 1;
 	i = 0;
 	new_line = -1;
-	while(read_ret != 0 && new_line == -1 && i < BUFF_SIZE * count)
+	read_ret = 3;
+	while(read_ret != 0 && new_line == -1 && i < (size_t)BUFF_SIZE * count)
 	{
 		ft_strclr(ret);
 		read_ret = read(fd, ret, 1);
@@ -50,11 +63,8 @@ static int gnl(char *ret, char **tmp, const int fd)
 		new_line = found_nl(ret);
 		if (new_line != -1)
 			ret[new_line] = '\0';	
-		if (i == BUFF_SIZE * count - 1)
-		{
-			count++;
-			*tmp = ft_strjoin(*tmp, ret);
-		} else *tmp = ft_strncat(*tmp, ret, BUFF_SIZE * count);
+		if (cat_join(&i, &count, tmp, ret) == - 1)
+			return (-1);
 		i++;
 	}
 	return (read_ret);
@@ -69,16 +79,15 @@ int	get_next_line(const int fd, char **line)
 	if (fd < 0)
 		return (-1);
 	if (line == NULL)
-		if ((line = (char **)malloc(sizeof(char *))) == NULL)
-			return (-1);
-	ret = ft_strnew(BUFF_SIZE);
-	tmp = ft_strnew(BUFF_SIZE);
+		return (-1);
+	ret = ft_strnew(BUFF_SIZE + 1);
+	tmp = ft_strnew(BUFF_SIZE + 1);
 	ft_strclr(tmp);
 	read_ret = gnl(ret, &tmp, fd);
-	*line = tmp;
-	free(ret);
 	if (read_ret == -1)
 		return (read_ret);
+	*line = tmp;
+	free(ret);
 	if (read_ret == 0)
 		return (0);
 	return (1);
--- get_next_line.h
+++ get_next_line.h
@@ -12,7 +12,7 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 3 
 
 int	get_next_line(const int fd, char **line);
 #endif
