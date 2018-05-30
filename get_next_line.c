96f1584c5f734700fd3ab1742fd7cf977d5fca17
Monkeli-Fred DILAPISHO <mdilapi@e5r5p1.wethinkcode.co.za>
2018-05-30 08:30:29 +0200

    Reworked version. Better understanding

--- get_next_line.c
+++ get_next_line.c
@@ -16,79 +16,41 @@
 #include <unistd.h>
 #include <stdio.h>
 
-static int	found_nl(const char *s)
-{
-	int i;
-
-	i = 0;
-	if (s == NULL)
-		return (-1);
-	while (s[i] != '\0')
-	{
-		if (s[i++] == '\n')
-			return (i - 1);
-	}
-	return (-1);
-}
-static int	cat_join(size_t *i, int *count, char **tmp, char *ret)
-{
-	if (*i == (size_t)(BUFF_SIZE * *count))
-	{
-		*count = *count + 1;
-		if ((*tmp = ft_strjoin(*tmp, ret)) == NULL)
-			return (-1);
-	} else 
-	{
-		*tmp = ft_strncat(*tmp, ret, BUFF_SIZE * *count);
-	}
-	return (1);
-}
-static int	gnl(char *ret, char **tmp, const int fd)
-{
-	int		count;
-	int		new_line;
-	int 	read_ret;
-	size_t	i;
-	
-	count = 1;
-	i = 0;
-	new_line = -1;
-	read_ret = 3;
-	while(read_ret != 0 && new_line == -1 && i < (size_t)BUFF_SIZE * count + 1)
-	{
-		ft_strclr(ret);
-		read_ret = read(fd, ret, 1);
-		if (read_ret == -1)
-			return (read_ret);
-		new_line = found_nl(ret);
-		if (new_line != -1)
-			ret[new_line] = '\0';	
-		if (cat_join(&i, &count, tmp, ret) == - 1)
-			return (-1);
-		i++;
-	}
-	return (read_ret);
-}
-
 int	get_next_line(const int fd, char **line)
 {
-	int		read_ret;
-	char	*ret;
-	char	*tmp;
-
-	if (fd < 0)
+	static char	buffer[BUFF_SIZE + 1];
+	static char *ptr;
+	char *tmp;
+	char *ret_line;
+	int	read_ret;
+	int newline = 0;
+	read_ret = 3;
+	if (line == NULL || fd < 0)
 		return (-1);
-	if (line == NULL)
-		return (-1);
-	ret = ft_strnew(BUFF_SIZE + 1);
-	tmp = ft_strnew(BUFF_SIZE + 1);
-	ft_strclr(tmp);
-	read_ret = gnl(ret, &tmp, fd);
-	if (read_ret == -1)
-		return (read_ret);
-	*line = tmp;
-	free(ret);
-	if (read_ret == 0)
-		return (0);
-	return (1);
+	ret_line = ft_strnew(BUFF_SIZE + 1);
+	ft_strclr(ret_line);
+	if (ft_isempty(buffer))
+	{
+		read_ret = read(fd, buffer, BUFF_SIZE);
+		if (read_ret == -1)
+			return (read_ret);
+		ptr = buffer;
+	}
+	tmp = ret_line;
+	while (newline == 0 && read_ret != -1)
+	{
+		ptr = ft_strchr(buffer, '\n');
+		if(ptr)
+		{
+			ret_line = ft_strjoin(ret_line, ft_strsub(buffer, 0, ptr - buffer));
+			newline = 1;
+		}
+		else 
+		{
+			ret_line = ft_strjoin(ret_line, buffer);
+			read_ret = read(fd, buffer, BUFF_SIZE);
+		}
+	}
+	*line = ret_line;
+	return (read_ret);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -12,7 +12,9 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 3 
+# define BUFF_SIZE 3
+# include "./libft/libft.h"
+
 
 int	get_next_line(const int fd, char **line);
 #endif
