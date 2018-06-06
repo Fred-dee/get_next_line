6b1afaa0e7b170af93b33ed535a23ee6c55c1976
Fred Dilapisho <fred.dilapisho@gmail.com>
2018-06-06 14:29:49 +0200

    Memory leak and Large file fail

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdilapi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/06/04 09:36:20 by mdilapi           #+#    #+#             */
-/*   Updated: 2018/06/04 09:36:21 by mdilapi          ###   ########.fr       */
+/*   Created: 2018/05/23 11:40:05 by mdilapi           #+#    #+#             */
+/*   Updated: 2018/05/23 11:40:06 by mdilapi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-static void		swapnfree(char **var, char *new_val)
+void		swapnfree(char **var, char *new_val)
 {
 	char	*tmp;
 
@@ -25,7 +25,7 @@ static void		swapnfree(char **var, char *new_val)
 	*var = tmp;
 }
 
-static char		*get_uptonl(char *s1)
+static char	*get_uptonl(char *s1)
 {
 	size_t	i;
 	char	*ret;
@@ -44,86 +44,60 @@ static char		*get_uptonl(char *s1)
 		ret[i] = '\0';
 	}
 	return (ret);
-}
+} 
 
-static char		*get_afternl(char *s1)
+
+static 	int	gnl(const int fd, char **ret_line, char **buffer)
 {
-	size_t i;
-	size_t j;
-	char *ret;
-
-	i = 0;
-	while (s1[i] != '\0' && s1[i] != '\n')
-		i++;
-	if (s1[i] == '\0' || s1[i] != '\n')
-		return (NULL);
-	i++;
-	j = 0;
-	if ((ret = (char *)malloc(sizeof(char) * ft_strlen(s1) - i + 2)) != NULL)
-	{
-		while(s1[i] != '\0')
-			ret[j++] = s1[i++];
-		ret[j] = '\0';
-	}
-	return (ret);
-}
-
-static int	gnl(const int fd, char **ret_line, char **buffer)
-{
-	char	*tmp;
 	int		nl;
 	int		read_ret;
 	char	*vrytmp;
 
 	nl = 0;
-	read_ret = 10;
+	read_ret = 3;
 	while (nl == 0 && read_ret > 0)
 	{
-		tmp = ft_strchr(*buffer, '\n');
-		if (tmp)
+		if (ft_strchr(*buffer, '\n') != NULL)
 		{
 			nl = 1;
 			vrytmp = get_uptonl(*buffer);
+			for (size_t i = 0; i < ft_strlen(vrytmp); i++)
+				*buffer = *buffer + 1;
+			*buffer = *buffer + 1;
 			swapnfree(ret_line, ft_strjoin(*ret_line, vrytmp));
-			if (*(tmp + 1) == '\0')
-			{
+			if ( **buffer == '\0')
 				ft_strclr(*buffer);
-			}
-			else
-			{
-				swapnfree(buffer, get_afternl(*buffer));
-			}
-			free(vrytmp);
 		}
 		else
 		{
 			swapnfree(ret_line, ft_strjoin(*ret_line, *buffer));
-			ft_strclr(*buffer);
 			read_ret = read(fd, *buffer, BUFF_SIZE);
 			buffer[0][read_ret] = '\0';
 		}
 	}
 	return (read_ret);
 }
+
 int			get_next_line(const int fd, char **line)
 {
-	char static	*buffers[1000];
+	char static	*buffer[100];
 	char		*ret_line;
 	int			read_ret;
 
 	if (line == NULL || fd < 0)
 		return (-1);
-	if (buffers[fd] == NULL)
-		buffers[fd] = ft_strnew(BUFF_SIZE + 1);
+	if (buffer[fd] == NULL)
+		buffer[fd] = ft_strnew(BUFF_SIZE + 1);
 	ret_line = ft_strnew(BUFF_SIZE + 1);
-	if (ft_isempty(buffers[fd]))
-	{
-		if((read_ret = read(fd, buffers[fd], BUFF_SIZE)) == -1)
-			return (-1);
-		buffers[fd][read_ret] = '\0';  
-	}
 	ft_strclr(ret_line);
-	if((read_ret = gnl(fd, &ret_line, &buffers[fd]) < 0))
+	if (ft_isempty(buffer[fd]))
+	{
+		if ((read_ret = read(fd, buffer[fd], BUFF_SIZE)) < 0)
+			return (-1);
+		buffer[fd][read_ret] = '\0';
+	}
+	read_ret = gnl(fd, &ret_line, &buffer[fd]);
+	if (read_ret < 0)
 		return (-1);
 	if (ft_isempty(ret_line) && read_ret == 0)
 		return (0);
